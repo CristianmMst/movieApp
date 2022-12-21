37a6008bea8e61c9e162ac79e7b0675d3ea6f5f7
CristianmMst <cristianmmst@gmail.com>
2022-12-20 21:04:18 -0500

    login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,3 +1,32 @@
+import "./Login.scss";
+import { FaUserAlt } from "react-icons/fa";
+
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="LoginForm">
+      <div className="Login">
+        <div className="Login-title">
+          <div className="Login-title-icon">
+            <FaUserAlt color="#3f4be1" />
+          </div>
+          <h4 className="Login-title-title">Iniciar Sesion</h4>
+        </div>
+        <form className="Form">
+          <input
+            type="email"
+            className="Form-input"
+            placeholder="Correo Electronico"
+          />
+          <input
+            type="password"
+            className="Form-input"
+            placeholder="Contraseña"
+          />
+          <button type="submit" className="Form-submit">
+            Entrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
